5bd0dc176dbe66437535cf9847a3808355a3b6aa
fresolina <74408325+fresolina@users.noreply.github.com>
2024-04-13 22:44:32 +0200

    Player summon walking

---
+++ Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] GameObject _summonWalkingPrefab;
+
+    CircleCollider2D _collider;
+    Rigidbody2D _rigidbody;
+
+    void Awake() {
+        _collider = GetComponent<CircleCollider2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePosition - (Vector2)transform.position);
+            // Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
+            Move(direction);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            SummonWalking();
+        }
+    }
+
+    public void Move(Vector2 direction) {
+        _rigidbody.AddForce(direction, ForceMode2D.Impulse);
+    }
+
+    void SummonWalking() {
+        Vector2 position = transform.position;
+        position.y += 0.99f;
+        GameObject anim = Instantiate(_summonWalkingPrefab, position, Quaternion.identity);
+        gameObject.SetActive(false);
+    }
+
+}
