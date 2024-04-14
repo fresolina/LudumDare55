353bb11e264cd50b5c774e6443300ad42a7adb86
fresolina <74408325+fresolina@users.noreply.github.com>
2024-04-14 22:26:52 +0200

    randomize goblin talk gap

--- Assets/_Project/Scripts/RandomAudioPlayer.cs
+++ Assets/_Project/Scripts/RandomAudioPlayer.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 public class RandomAudioPlayer : MonoBehaviour {
     [SerializeField] AudioClip[] audioClips;
     AudioSource audioSource;
+    bool _audioQueued;
 
     void Awake() {
         audioSource = GetComponent<AudioSource>();
         audioSource.loop = false;
         audioSource.enabled = true;
-        PlayRandomClip();
+        _audioQueued = false;
     }
 
     void Update() {
-        if (!audioSource.isPlaying) {
-            PlayRandomClip();
+        if (!audioSource.isPlaying && !_audioQueued) {
+            // Wait 3-6 seconds before playing another clip
+            Invoke("PlayRandomClip", Random.Range(3f, 6f));
+            _audioQueued = true;
         }
     }
 
