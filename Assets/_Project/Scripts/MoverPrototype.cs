d8cc470cf60494c1344b4aa4721e4747597ececa
Björn Weinehall <gecko+github@acc.umu.se>
2024-04-14 15:45:30 +0200

    Walking and collision detection

--- Assets/_Project/Scripts/MoverPrototype.cs
+++ Assets/_Project/Scripts/MoverPrototype.cs
@@ -6,9 +6,18 @@ public class MoverPrototype : MonoBehaviour {
     public float speed = 1.0f;
     private Vector3 direction = Vector3.zero;
 
+    private Vector3 collisionDirection = Vector3.zero;
+    private bool stuck = false;
+
     public void Update() {
         transform.parent.position += direction * speed * Time.deltaTime;
 
+        if (direction != Vector3.zero) {
+            collisionDirection = Vector3.zero;
+            stuck = false;
+        }
+
+        // TODO: disable dev cheatcodes (?)
         if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             CastTurnLeft();
         } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
@@ -19,14 +28,24 @@ public class MoverPrototype : MonoBehaviour {
     }
 
     public void CastTurnLeft() {
-        direction = Vector3.left;
+        if (collisionDirection != Vector3.left && !stuck)
+            direction = Vector3.left;
     }
 
     public void CastTurnRight() {
-        direction = Vector3.right;
+        if (collisionDirection != Vector3.right && !stuck)
+            direction = Vector3.right;
     }
 
     public void CastUnsummon() {
         GetComponentInParent<CarrierController>().Unsummon();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        // print("Collision triggered!");
+        if (direction == Vector3.zero)
+            stuck = true;
+        collisionDirection = direction;
+        direction = Vector3.zero;
     }
 }
