40cd85a0596931ae99a0281b39f524a2a5a37b85
fresolina <74408325+fresolina@users.noreply.github.com>
2024-04-15 20:14:11 +0200

    Remove mousclick to move ball

--- Assets/_Project/Scripts/PlayerDebugInputController.cs
+++ Assets/_Project/Scripts/PlayerDebugInputController.cs
@@ -5,12 +5,6 @@ public class PlayerDebugInputController : MonoBehaviour {
     [SerializeField] Player _player;
 
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 direction = (mousePosition - (Vector2)transform.position);
-            _player.Move(direction);
-        }
-
         // Debug inputs
         if (Input.GetKeyDown(KeyCode.Alpha1)) {
             if (_player.gameObject.activeSelf && !_player.IsHidden()) {
