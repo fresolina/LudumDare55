99f1a7853fbab5c1fa0a37f1dd4b423810cb8f87
Björn Weinehall <gecko+github@acc.umu.se>
2024-04-15 22:57:43 +0200

    Add more tutorial text

--- Assets/_Project/Scripts/Spawnpoint.cs
+++ Assets/_Project/Scripts/Spawnpoint.cs
@@ -3,14 +3,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Spawnpoint : MonoBehaviour {
-    private OverlayController _overlayController;
-    private AudioSource _audioSource;
-
     public GameObject _prefab;
 
     void Start() {
-        _overlayController = OverlayController.Instance();
-        _audioSource = GetComponent<AudioSource>();
+        var _overlayController = OverlayController.Instance();
+        var _audioSource = GetComponent<AudioSource>();
 
         _overlayController.ShowSummonPalette(true);
         _overlayController.ShowTypeIndicator(true);
@@ -30,6 +27,9 @@ public class Spawnpoint : MonoBehaviour {
             case "Tutorial_1":
                 OverlayController.Instance().LimitSpells(1);
                 OverlayController.Instance().AddMessage("message", "You are trapped and can't move yourself\nSummon your trusty servants for help.\nType their name now!\nThen tell them to carry you left.\nFinally unsummon them when touching the flag.");
+                break;
+            case "Tutorial_2":
+                OverlayController.Instance().AddMessage("message", "To get across bumps and gaps, use the cannon!\nCareful, or your ball will break!");
                 break;
         }
     }
