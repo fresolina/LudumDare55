eb1d5314b44636017f83f5f4bb45db06b185e6d3
fresolina <74408325+fresolina@users.noreply.github.com>
2024-04-15 22:45:57 +0200

    Loop all music

--- Assets/_Project/Scripts/MusicPlayer.cs
+++ Assets/_Project/Scripts/MusicPlayer.cs
@@ -13,29 +13,26 @@ public class MusicPlayer : MonoBehaviour {
     void Awake() {
         Instance = this;
         _audioSource = GetComponent<AudioSource>();
+        _audioSource.loop = true;
     }
 
     public void PlayTitle() {
         _audioSource.clip = _title;
-        _audioSource.loop = true;
         _audioSource.Play();
     }
 
     public void PlayGameInProgress() {
         _audioSource.clip = _gameInProgress;
-        _audioSource.loop = true;
         _audioSource.Play();
     }
 
     public void PlayGameWon() {
         _audioSource.clip = _gameWon;
-        _audioSource.loop = false;
         _audioSource.Play();
     }
 
     public void PlayGameOver() {
         _audioSource.clip = _gameOver;
-        _audioSource.loop = false;
         _audioSource.Play();
     }
 
